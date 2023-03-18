f4fa496e018c8ba57e493f2faece792d8fb52956
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-03-18 14:04:34 +0530

    Hotel controller all added

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -10,37 +10,18 @@ router.post("/", createHotel);
 
 // UPDATE
 router.put("/:id", async (req, res) => {
-  try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(
-      req.params.id,
-      { $set: req.body },
-      { new: true }
-    );
-    res.status(200).json(updatedHotel);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  
 });
 
 // DELETE
 
 router.delete("/:id", async (req, res) => {
-  try {
-    await Hotel.findByIdAndDelete(req.params.id);
-    res.status(200).json("Hotel has been deleted");
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  
 });
 
 // GET
 router.get("/:id", async (req, res) => {
-  try {
-    const hotel = await Hotel.findById(req.params.id);
-    res.status(200).json(hotel);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+ 
 });
 
 // GET ALL
@@ -52,12 +33,7 @@ router.get("/", async (req, res, next) => {
 //     return next(createError(404, "You are not authenticated"));
 //   }
 
-  try {
-    const hotels = await Hotel.find();
-    res.status(200).json(hotels);
-  } catch (err) {
-    next(err);
-  }
+ 
 });
 
 export default router;
