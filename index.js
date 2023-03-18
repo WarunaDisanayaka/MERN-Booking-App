9b9320b159b8fffc7fa2604fdf7bbf75bede4603
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-03-18 10:55:46 +0530

    Initial commit(Connect mongodb)

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+const app = express();
+dotenv.config();
+
+// MongoDB Connection
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB...");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected",()=>{
+    console.log("Mongodb Disconnected");
+})
+
+mongoose.connection.on("connected",()=>{
+    console.log("Mongodb connected!");
+})
+
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend");
+});
