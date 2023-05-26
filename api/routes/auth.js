7954dfea858d4a79360452f1004b38e0f22db203
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-05-27 01:48:02 +0530

    Login added

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { register } from "../controller/authController.js";
+import { login, register } from "../controller/authController.js";
 
 const router = express.Router();
 
 router.post("/register",register);
-
+router.post("/login",login);
 
 
 
