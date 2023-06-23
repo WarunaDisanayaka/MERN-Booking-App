99028fdb8b9f56a88ec1a0d21112d6e7829ab739
Waruna <warunahello@gmail.com>
2023-06-23 13:00:43 +0530

    authenticating fixed

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,12 @@
-import { updateUser,deleteUser,getUser,getUsers } from "../controller/userController.js";
 import express from "express";
+import { updateUser,deleteUser,getUser,getUsers } from "../controller/userController.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication",verifyToken,(req,res,next)=>{
+    res.send("Hello user, you are logged in");
+})
 
 // UPDATE
 router.put("/:id", updateUser);
