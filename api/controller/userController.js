6c3f8553ae573e82065747f0981177ea19b5af68
Waruna <warunahello@gmail.com>
2023-06-23 12:32:29 +0530

    user routes and controllers added

---
+++ api/controller/userController.js
@@ -0,0 +1,45 @@
+import User from "../models/User.js";
+
+// Update User
+export const updateUser = async (req, res, next) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// Delete User
+export const deleteUser = async (req, res, next) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// Get User(One)
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// Get Users(All)
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,10 +1,21 @@
+import { updateUser,deleteUser,getUser,getUsers } from "../controller/userController.js";
 import express from "express";
 
 const router = express.Router();
 
+// UPDATE
+router.put("/:id", updateUser);
 
+// DELETE
 
+router.delete("/:id", deleteUser);
 
+// GET
+router.get("/:id", getUser);
+
+// GET ALL
+
+router.get("/", getUsers);
 
 
 export default router;
