c21cfad7f0c14eebb955082be464dfe10ef62a63
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-07-14 03:01:39 +0530

    Room controller and router added

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,8 +1,23 @@
 import express from "express";
+import { createRoom,updateRoom,deleteRoom,getRoom,getRooms } from "../controller/roomController.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// Create
+router.post("/:hotelid",verifyToken,createRoom);
 
+// Update
+router.put("/:id",verifyToken,updateRoom);
+
+// Delete
+router.delete("/:id",verifyToken,deleteRoom);
+
+// Get one room
+router.get("/:id",getRoom);
+
+// Get rooms
+router.get("/",getRooms);
 
 
 export default router;
