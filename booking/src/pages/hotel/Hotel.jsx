51b40e85584c2a3311e06e5edaf532310fb6a76b
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-07-15 00:02:50 +0530

    Add router path and page components

---
+++ booking/src/pages/hotel/Hotel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function hotel() {
+  return (
+    <div>hotel</div>
+  )
+}
+
+export default hotel
---
+++ booking/src/pages/list/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const List = () => {
+  return (
+    <div>
+      Hotels
+    </div>
+  )
+}
+
+export default List
