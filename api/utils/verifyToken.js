fa854f1a4156778b31e7ea6a584b07612be98b18
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2024-01-07 15:09:26 +0530

    Added verifyUser function

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -13,3 +13,15 @@ export const verifyToken = (req,res,next) => {
     next()
    }); 
 };
+
+export const verifyUser = (req,res,next) => {
+    verifyToken(req,res,next,()=>{
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            next()
+        }else{
+            if (err) {
+                return next(createError(403,"You are not authorized!"));
+            }
+        }
+    })
+}
