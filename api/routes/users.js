f01c1d789bc50eab68055e78e6f06d2468a086dc
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2024-01-10 09:14:42 +0530

    Updated user route and verifyuser

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { updateUser,deleteUser,getUser,getUsers } from "../controller/userController.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/checkauthentication",verifyToken,(req,res,next)=>{
     res.send("Hello user, you are logged in");
+})
+
+router.get("/checkuser/:id",verifyUser,(req,res,next)=>{
+    res.send("Hello user, you are logged in and you can delete your account");
 })
 
 // UPDATE
--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -15,9 +15,9 @@ export const verifyToken = (req,res,next) => {
 };
 
 export const verifyUser = (req,res,next) => {
-    verifyToken(req,res,next,()=>{
+    verifyToken(req,res,()=>{
         if (req.user.id === req.params.id || req.user.isAdmin) {
-            next()
+            next();
         }else{
             if (err) {
                 return next(createError(403,"You are not authorized!"));
