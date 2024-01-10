c9a3e4678797f64713203ce835ba28581be5dda0
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2024-01-10 10:15:16 +0530

    Added middleware to room routes

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,17 +1,17 @@
 import express from "express";
 import { createRoom,updateRoom,deleteRoom,getRoom,getRooms } from "../controller/roomController.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 // Create
-router.post("/:hotelid",verifyToken,createRoom);
+router.post("/:hotelid",verifyAdmin,createRoom);
 
 // Update
-router.put("/:id",verifyToken,updateRoom);
+router.put("/:id",verifyAdmin,updateRoom);
 
 // Delete
-router.delete("/:id",verifyToken,deleteRoom);
+router.delete("/:id",verifyAdmin,deleteRoom);
 
 // Get one room
 router.get("/:id",getRoom);
