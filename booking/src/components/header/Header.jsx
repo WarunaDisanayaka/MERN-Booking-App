8665ee3ae922aaa6cd0a72b7e8077760aa49f3fd
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2024-01-14 10:44:50 +0530

    Header design done

--- booking/src/components/header/Header.jsx
+++ booking/src/components/header/Header.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import "./header.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBed, faCar, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+
+
 
 const Header = () => {
-  return (
-    <div className='hedaer'>Header</div>
-  )
+    return (
+        <div className='header'>
+            <div className="headerContainer">
+                <div className="headerList">
+                    <div className="headerListItem active">
+                        <FontAwesomeIcon icon={faBed} />
+                        <span>Stays</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faPlane} />
+                        <span>Flights</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faCar} />
+                        <span>Flights</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faTaxi} />
+                        <span>Flights</span>
+                    </div>
+                </div>
+                <h1 className="headerTitle">A lifetime of discounts? It's Genius</h1>
+                <p className="headerDesc">Get rewarded for your travels unlock instant savings of 10% or 
+                more with a free Booking account</p>
+                <button className="headerBtn">Sign in / Register</button>
+            </div>
+        </div>
+    )
 }
 
 export default Header;
