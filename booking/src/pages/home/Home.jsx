a51b5513272c00e103bbe0f297e0942234f02b9c
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2024-08-01 15:40:20 +0530

    Added featured properties

--- booking/src/pages/home/Home.jsx
+++ booking/src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
 import PropertyList from '../../components/propertyList/PropertyList'
@@ -16,7 +17,7 @@ const Home = () => {
         <PropertyList />
 
         <h1 className='homeTitle'>Homes guests love</h1>
-        
+        <FeaturedProperties />
       </div>
     </div>
   )
